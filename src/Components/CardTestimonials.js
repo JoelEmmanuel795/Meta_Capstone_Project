541c2df87e351becee74af4360185335990a6c50
Joel Gamonez <j.e.h.gamonez@gmail.com>
2025-02-01 03:18:08 +0100

    Edited card format to look more like the prototype

--- src/Components/CardTestimonials.js
+++ src/Components/CardTestimonials.js
@@ -4,9 +4,11 @@ import '../CSS/CardTestimonials.css';
 const CardTestimonials = ({ image, name, review, rating }) => {
   return (
     <div className="testimonial-card">
-      <img src={image} alt={name} className="testimonial-image" />
-      <h5 className="testimonial-name">{name}</h5>
       <p className="testimonial-rating">{rating}</p>
+      <div className="testimonial-flex">
+        <img src={image} alt={name} className="testimonial-image" />
+        <h5 className="testimonial-name">{name}</h5>
+      </div>
       <p className="testimonial-review">{review}</p>
     </div>
   );
